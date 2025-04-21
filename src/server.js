a876e8f14dc94e78f3827647625c538237b97f5d
Ira Panasiuk <panasykira2022@gmail.com>
2025-04-22 00:07:20 +0300

    update _api_docs

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { UPLOAD_DIR } from './constans/index.js';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -23,6 +24,7 @@ export const startServer = () => {
   app.use('/wallet', walletRouter);
   app.use('/user', userRouter);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(errorHandler);
   app.all(/.*/, notFoundHandler);
 
