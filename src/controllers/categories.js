9c087c3f43d7fbb9f1348ea5bb36ddda2d0966a3
Ira Panasiuk <panasykira2022@gmail.com>
2025-04-27 21:58:40 +0300

    swagger

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,19 +1,11 @@
-import createHttpError from 'http-errors';
+import { getAllCategories } from '../services/categories.js';
 
-import { getCategoryById } from '../services/categories.js';
-
-export const getCategoryByIdController = async (req, res, next) => {
-  const { categoryId } = req.params;
-
-  const category = await getCategoryById(categoryId);
-
-  if (!category) {
-    throw createHttpError(404, `Category with ID ${categoryId} not found!`);
-  }
+export const getAllCategoriesController = async (req, res, next) => {
+  const categories = await getAllCategories();
 
   res.status(200).json({
     status: 200,
-    message: `Successfully found category with ID ${categoryId}!`,
-    data: category,
+    message: 'Successfully found all categories!',
+    categories,
   });
 };
